d9d6e8d22a4ebcd37517aef6ef4b79d404b51a11
tylertruong <tylertruong@gmail.com>
2017-09-08 15:16:23 -0700

    Add ability to request data from youtube.

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,23 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    type: 'GET',
+    data: {
+      'key': options.key, 
+      'maxResults': options.max,
+      'part': 'snippet',
+      'q': options.query,
+      'type': 'video',
+      'videoEmbeddable': 'true'},
+    success: data => {
+      console.log('youtube: Got the Data', data);
+      callback(data.items);
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('youtube: Failed to get data', data);
+    }
+  });
 };
 
 window.searchYouTube = searchYouTube;
